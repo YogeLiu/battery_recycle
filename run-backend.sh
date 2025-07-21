156cda91bf8fb4dd13027c6baa2795c66367f12e
liuyuanchuang <yuanchuang_liu@qingsongchou.com>
2025-07-21 14:28:54 +0800

    fix: page_size

--- run-backend.sh
+++ run-backend.sh
@@ -4,7 +4,7 @@
 export DB_HOST=182.92.150.161
 export DB_PORT=3006
 export DB_USER=root
-export DB_PASSWORD="yoge@coder%%%123321!"
+export DB_PASSWORD="yoge@coder%%%123321!"   
 export DB_NAME=battery_recycle_erp
 export SEED_DATABASE=true
 export JWT_SECRET=your_jwt_secret_key
